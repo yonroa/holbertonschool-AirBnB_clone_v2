cb0cb9dcf521a3981bcb1e9b7d96c173658b3b6f
yonroa <yonanderson11@gmail.com>
2022-09-13 16:53:35 -0500

    Change in task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Print an specified text after the letter C"""
-    return f"C {text.replace('_', ' ')}"
+    return "C {}".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
